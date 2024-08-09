d2ff2411fc51931212ae208138c1c740e9ce03b1
Neha Nihar <niharneha.1997@gmail.com>
2024-08-09 18:20:52 +0530

    Added prepared query to run dynamic query that can be used multiple times and resultset metadata

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -26,10 +26,40 @@ public class JDBCDemo {
 
         ResultSet resultSet = statement.executeQuery(sql);
         while(resultSet.next()){
-            System.out.println("Id is : "+ resultSet.getInt("id")+"\t");
-            System.out.println("Name is : "+ resultSet.getString("name")+"\t");
-            System.out.println("Aadhar number is : "+ resultSet.getString("aadhar_id")+"\t");
-            System.out.println();
+//            System.out.println("Id is : "+ resultSet.getInt("id")+"\t");
+//            System.out.println("Name is : "+ resultSet.getString("name")+"\t");
+//            System.out.println("Aadhar number is : "+ resultSet.getString("aadhar_id")+"\t");
+//            System.out.println();
         }
+        /*
+         * Prepared statement
+         * */
+
+        System.out.println("demo of prepared statement");
+        String preSQL = "select id,name,gender,aadhar_id,is_nri from citizens where name=? and gender=?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(preSQL);
+        preparedStatement.setString(1,"anil");
+        preparedStatement.setString(2,"Male");
+
+        resultSet = preparedStatement.executeQuery();
+
+        /*
+        * Explore metadata about result set
+        * */
+
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        System.out.println("Total columns: "+ resultSetMetaData.getColumnCount());
+        System.out.println("Name of 1st columns: "+ resultSetMetaData.getColumnName(1));
+//        while(resultSet.next()){
+//            System.out.println("Id is : "+ resultSet.getInt("id")+"\t");
+//            System.out.println("Name is : "+ resultSet.getString("name")+"\t");
+//            System.out.println("Aadhar number is : "+ resultSet.getString("aadhar_id")+"\t");
+//            System.out.println();
+//        }
+
+        preparedStatement.close();
+        connection.close();
+
     }
 }
